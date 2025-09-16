a40bbd80dbb5ba21ad5ed5834270ff95e88e93a1
Heatless <dianwilson66@gmail.com>
2025-09-16 01:04:00 +0100

    Update VideoCard.js

--- components/VideoCard.js
+++ components/VideoCard.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { supabase } from '../lib/supabase'
+import { supabase } from '../lib/supabaseClient'
 
 export default function VideoFeed() {
   const [videos, setVideos] = useState([])
@@ -34,4 +34,4 @@ export default function VideoFeed() {
       ))}
     </div>
   )
-              }
+}
