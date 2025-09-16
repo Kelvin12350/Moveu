672f3081ee7a7af040442c2862a86eb685557eb9
Heatless <dianwilson66@gmail.com>
2025-09-16 18:27:48 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
-// pages/_app.js
-import React from 'react'
-import '../styles/globals.css' // <-- fixed relative import
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
