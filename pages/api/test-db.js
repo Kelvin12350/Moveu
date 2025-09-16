60fcf8f50d101d36c72762e533776430b27fe314
Heatless <dianwilson66@gmail.com>
2025-09-16 20:48:35 +0100

    Update test-db.js

--- pages/api/test-db.js
+++ pages/api/test-db.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "API is working fine ✅" });
+  res.status(200).json({ message: "Database connection test successful!" });
 }
