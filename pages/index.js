fb035c6651c96b03e2ed1fc2588f4f24b1680686
Heatless <dianwilson66@gmail.com>
2025-09-16 03:47:00 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
-// pages/index.js
-import React from "react";
+import VideoFeed from '../components/VideoFeed';
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <video
-        src="https://res.cloudinary.com/ds9cmu7sa/video/upload/v1757988413/c5845c1d50fc71193aef97eff1c4f66d_tk2cpp.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="h-screen w-auto object-contain"
-      />
+    <div className="app">
+      <VideoFeed />
     </div>
   );
 }
