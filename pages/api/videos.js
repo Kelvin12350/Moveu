9e44309e9e40d8db3616179e45b46b245e1dc01c
Heatless <dianwilson66@gmail.com>
2025-09-16 15:28:59 +0100

    Update videos.js

--- pages/api/videos.js
+++ pages/api/videos.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
 });
 
 export default async function handler(req, res) {
@@ -14,8 +14,9 @@ export default async function handler(req, res) {
       .max_results(30)
       .execute();
 
-    res.status(200).json(result);
+    res.status(200).json(result.resources);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch videos" });
   }
 }
