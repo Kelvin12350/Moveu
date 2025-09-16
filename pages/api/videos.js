b655a6a7344634f0ca0799e9a8e9b3d31d6688fb
Heatless <dianwilson66@gmail.com>
2025-09-16 20:36:07 +0100

    Update videos.js

--- pages/api/videos.js
+++ pages/api/videos.js
@@ -1,24 +1,26 @@
-import axios from "axios";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export default async function handler(req, res) {
-  try {
-    const { data } = await axios.get(
-      `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/resources/search`,
-      {
-        auth: {
-          username: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-          password: process.env.CLOUDINARY_API_SECRET,
-        },
-        params: {
-          expression: "resource_type:image OR resource_type:video",
-          max_results: 50,
-        },
-      }
-    );
+  if (req.method === "GET") {
+    try {
+      const { resources } = await cloudinary.search
+        .expression("folder:moveu_media AND resource_type:video")
+        .sort_by("created_at", "desc")
+        .max_results(20)
+        .execute();
 
-    res.status(200).json(data.resources);
-  } catch (error) {
-    console.error("Error fetching Cloudinary resources:", error);
-    res.status(500).json({ error: "Failed to load resources" });
+      res.status(200).json(resources);
+    } catch (error) {
+      console.error("Fetch Error:", error);
+      res.status(500).json({ error: "Failed to fetch videos" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
