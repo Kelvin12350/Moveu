00e84cb91522926fa7f06a749953d45f24c6aa2a
Heatless <dianwilson66@gmail.com>
2025-09-16 03:28:53 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
+// pages/index.js
+import React from "react";
+
 export default function Home() {
   return (
-    <div className="container">
-      <video className="video-player" autoPlay loop muted playsInline>
-        <source
-          src="https://res.cloudinary.com/ds9cmu7sa/video/upload/v1757988413/c5845c1d50fc71193aef97eff1c4f66d_tk2cpp.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
+    <div className="flex items-center justify-center h-screen bg-black">
+      <video
+        src="https://res.cloudinary.com/ds9cmu7sa/video/upload/v1757988413/c5845c1d50fc71193aef97eff1c4f66d_tk2cpp.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="h-screen w-auto object-contain"
+      />
     </div>
   );
 }
