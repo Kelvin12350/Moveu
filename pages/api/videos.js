2dc305294fd785b7002436c783767ed2c7da6ac6
Heatless <dianwilson66@gmail.com>
2025-09-16 03:54:34 +0100

    Update videos.js

--- pages/api/videos.js
+++ pages/api/videos.js
@@ -1,10 +1,22 @@
+let videos = [
+  {
+    id: 1,
+    title: "Demo Video",
+    url: "https://res.cloudinary.com/ds9cmu7sa/video/upload/v1757988413/c5845c1d50fc71193aef97eff1c4f66d_tk2cpp.mp4",
+  },
+];
+
 export default function handler(req, res) {
-  res.status(200).json([
-    {
-      id: 1,
-      title: "Cloudinary Demo Video",
-      url: "https://res.cloudinary.com/ds9cmu7sa/video/upload/v1757988413/c5845c1d50fc71193aef97eff1c4f66d_tk2cpp.mp4",
-      thumbnail: "https://img.youtube.com/vi/YE7VzlLtp-4/hqdefault.jpg"
-    }
-  ]);
+  if (req.method === "POST") {
+    const { url } = req.body;
+    const newVideo = {
+      id: videos.length + 1,
+      title: "User Upload",
+      url,
+    };
+    videos.push(newVideo);
+    return res.status(200).json(newVideo);
+  }
+
+  res.status(200).json(videos);
 }
